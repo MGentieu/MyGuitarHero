e3c6c22cf5457e9d5925c79360bb5e2163983119
MGentieu <mgentieu02@gmail.com>
2024-11-14 09:06:05 +0100

    Création de la branche pour Martin

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import pygame
 import pygame.gfxdraw 
 
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-screen.fill((255, 0, 0))
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((500, 500))
+    screen.fill((255, 0, 0))
+
+    pygame.display.flip()
+    try:
+        while True:
+            event = pygame.event.wait()
+            if event.type == pygame.QUIT:
+                break
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE or event.unicode == "q":
+                    break
+            pygame.display.flip()
+    finally:
+        pygame.quit()
