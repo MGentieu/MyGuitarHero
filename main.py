01a1c661b6c02e0037368f10187fdd77d731a2fc
MGentieu <mgentieu02@gmail.com>
2024-11-14 09:40:31 +0100

    Test écran

--- main.py
+++ main.py
@@ -1,12 +1,51 @@
 import pygame
 import pygame.gfxdraw 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((500, 500))
-    screen.fill((255, 0, 0))
+import numpy as np
+import tkinter as tk
 
+def VertGradientColumn(surf, topcolor, bottomcolor):
+    "creates a new 3d vertical gradient array"
+    topcolor = np.array(topcolor, copy=False)
+    bottomcolor = np.array(bottomcolor, copy=False)
+    diff = bottomcolor - topcolor
+    width, height = surf.get_size()
+    # create array from 0.0 to 1.0 triplets
+    column = np.arange(height, dtype="float") / height
+    column = np.repeat(column[:, np.newaxis], [3], 1)
+    # create a single column of gradient
+    column = topcolor + (diff * column).astype("int")
+    # make the column a 3d image column by adding X
+    column = column.astype("uint8")[np.newaxis, :, :]
+    # 3d array into 2d array
+    return pygame.surfarray.map_array(surf, column)
+
+def DisplayGradient(surf):
+    "choose random colors and show them"
+    #colors = np_random.randint(0, 255, (2, 3))
+    colors = [[255, 255, 255], [55, 55, 55]]
+    column = VertGradientColumn(surf, colors[0], colors[1])
+    pygame.surfarray.blit_array(surf, column)
     pygame.display.flip()
+
+def main():
+    
+
+# Création d'une fenêtre tkinter
+    root = tk.Tk()
+    root.withdraw()  # Cache la fenêtre tkinter
+
+# Récupération de la largeur et de la hauteur de l'écran
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+
+    pygame.init()
+    size = screen_width, screen_height
+    screen = pygame.display.set_mode(size, pygame.NOFRAME, 0)
+    #screen.fill((10, 10, 55))
+
+    DisplayGradient(screen)
     try:
         while True:
             event = pygame.event.wait()
@@ -15,6 +54,9 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE or event.unicode == "q":
                     break
-            pygame.display.flip()
+            
     finally:
         pygame.quit()
+        
+if __name__ == "__main__":
+    main()
